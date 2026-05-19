956019931b3b262c94fa3993dd5af49ba401bd2c
WillEdSanchez <williansnttdata24@gmail.com>
2026-05-19 14:22:45 -0500

    Configure Terraform remote backend

--- provider.tf
+++ provider.tf
@@ -2,6 +2,12 @@ terraform {
 
   required_version = ">= 1.5"
 
+  backend "s3" {
+    bucket = "terraform-state-willians-2026"
+    key    = "terraform-ec2-lab/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
